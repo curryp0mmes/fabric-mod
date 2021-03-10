f61e7513ba8e796e2acede4866282368e945a2e1
curryp0mmes <34415605+curryp0mmes@users.noreply.github.com>
2021-03-10 09:13:46 +0100

    added Crystal Armor

--- src/main/java/com/curryp0mmes/fabric/mod/uno/registry/ModItems.java
+++ src/main/java/com/curryp0mmes/fabric/mod/uno/registry/ModItems.java
@@ -1,6 +1,10 @@
 package com.curryp0mmes.fabric.mod.uno.registry;
 
+import com.curryp0mmes.fabric.mod.uno.CrystalArmorMaterial;
 import com.curryp0mmes.fabric.mod.uno.FabricMod;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
@@ -14,6 +18,13 @@ public class ModItems {
     public static final Item CRYSTAL = new Item(new Item.Settings().group(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_GROUP));
     public static final Item CRYSTAL_SHARD = new Item(new Item.Settings().group(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_GROUP));
 
+    //Armor
+    public static final ArmorMaterial CRYSTAL_ARMOR_MATERIAL = new CrystalArmorMaterial();
+    public static final Item CRYSTAL_HELMET = new ArmorItem(CRYSTAL_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(FabricMod.MOD_GROUP));
+    public static final Item CRYSTAL_CHESTPLATE = new ArmorItem(CRYSTAL_ARMOR_MATERIAL, EquipmentSlot.CHEST, new Item.Settings().group(FabricMod.MOD_GROUP));
+    public static final Item CRYSTAL_LEGGINGS = new ArmorItem(CRYSTAL_ARMOR_MATERIAL, EquipmentSlot.LEGS, new Item.Settings().group(FabricMod.MOD_GROUP));
+    public static final Item CRYSTAL_BOOTS = new ArmorItem(CRYSTAL_ARMOR_MATERIAL, EquipmentSlot.FEET, new Item.Settings().group(FabricMod.MOD_GROUP));
+
     //BlockItems
     public static final BlockItem STALIN_BLOCK = new BlockItem(ModBlocks.STALIN_BLOCK, new Item.Settings().group(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_GROUP));
     public static final BlockItem CRYSTAL_ORE = new BlockItem(ModBlocks.CRYSTAL_ORE, new Item.Settings().group(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_GROUP));
@@ -24,5 +35,10 @@ public class ModItems {
         Registry.register(Registry.ITEM, new Identifier(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_ID, "crystal_shard"), CRYSTAL_SHARD);
         Registry.register(Registry.ITEM, new Identifier(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_ID, "stalin_block"), STALIN_BLOCK);
         Registry.register(Registry.ITEM, new Identifier(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_ID, "crystal_ore"), CRYSTAL_ORE);
+
+        Registry.register(Registry.ITEM, new Identifier(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_ID, "crystal_helmet"), CRYSTAL_HELMET);
+        Registry.register(Registry.ITEM, new Identifier(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_ID, "crystal_chestplate"), CRYSTAL_CHESTPLATE);
+        Registry.register(Registry.ITEM, new Identifier(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_ID, "crystal_leggings"), CRYSTAL_LEGGINGS);
+        Registry.register(Registry.ITEM, new Identifier(com.curryp0mmes.fabric.mod.uno.FabricMod.MOD_ID, "crystal_boots"), CRYSTAL_BOOTS);
     }
 }
