af758bc855a618af820ef0a333ae984fa44bd668
curryp0mmes <34415605+curryp0mmes@users.noreply.github.com>
2021-03-13 12:40:42 +0100

    fixed negative ammo and server client desync

--- src/main/java/com/curryp0mmes/fabric/mod/uno/customstuff/GunItem.java
+++ src/main/java/com/curryp0mmes/fabric/mod/uno/customstuff/GunItem.java
@@ -31,34 +31,32 @@ public class GunItem extends Item {
             user.playSound(SoundEvents.UI_BUTTON_CLICK, 1F, 1F);
             user.sendMessage(new TranslatableText("item.curry.pistol.no_ammo"), true);
             return TypedActionResult.fail(itemStack);
-        } else {
-            if (world.isClient && !user.abilities.creativeMode) {
-
-                System.out.println(ammo + 15);
-                ammo--;
-                tag.putInt("ammunition", ammo);
-                itemStack.setTag(tag);
-                user.sendMessage(new TranslatableText("item.curry.pistol.ammo_left").append(String.valueOf(ammo)), true);
-            }
-
-            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_GENERIC_EXPLODE, SoundCategory.BLOCKS, 1F, 2F); // plays a globalSoundEvent
-            user.getItemCooldownManager().set(this, 20); //cooldown to use it again
-        /*
-		user.getItemCooldownManager().set(this, 5);
-		Optionally, you can add a cooldown to your item's right-click use, similar to Ender Pearls.
-		*/
-            if (!world.isClient) {
-                GunProjectile gunProjectile = new GunProjectile(world, user);
-                gunProjectile.setItem(itemStack);
-                gunProjectile.setProperties(user, user.pitch, user.yaw, 0.0F, 5.5F, 0F);
-                world.spawnEntity(gunProjectile); // spawns entity
-            }
-
-            user.incrementStat(Stats.USED.getOrCreateStat(this));
-
-
-            return TypedActionResult.success(itemStack, world.isClient());
-
         }
+        if (!user.abilities.creativeMode) {
+            ammo--;
+            tag.putInt("ammunition", ammo);
+            itemStack.setTag(tag);
+            user.sendMessage(new TranslatableText("item.curry.pistol.ammo_left").append(String.valueOf(ammo+15)), true);
+        }
+
+        world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_GENERIC_EXPLODE, SoundCategory.BLOCKS, 1F, 2F); // plays a globalSoundEvent
+        user.getItemCooldownManager().set(this, 20); //cooldown to use it again
+    /*
+    user.getItemCooldownManager().set(this, 5);
+    Optionally, you can add a cooldown to your item's right-click use, similar to Ender Pearls.
+    */
+        if (!world.isClient) {
+            GunProjectile gunProjectile = new GunProjectile(world, user);
+            gunProjectile.setItem(itemStack);
+            gunProjectile.setProperties(user, user.pitch, user.yaw, 0.0F, 5.5F, 0F);
+            world.spawnEntity(gunProjectile); // spawns entity
+        }
+
+        user.incrementStat(Stats.USED.getOrCreateStat(this));
+
+
+        return TypedActionResult.success(itemStack, world.isClient());
+
     }
+
 }
