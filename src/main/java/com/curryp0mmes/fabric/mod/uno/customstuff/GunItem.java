bf8a0bb960af903a62bda7bef28703e32ea1b73f
curryp0mmes <34415605+curryp0mmes@users.noreply.github.com>
2021-03-13 12:35:08 +0100

    ammo is now stored as nbt

--- src/main/java/com/curryp0mmes/fabric/mod/uno/customstuff/GunItem.java
+++ src/main/java/com/curryp0mmes/fabric/mod/uno/customstuff/GunItem.java
@@ -16,7 +16,7 @@ import java.awt.*;
 import java.util.List;
 
 public class GunItem extends Item {
-    private int ammunition = 10;
+
     public GunItem(Settings settings) {
         super(settings);
     }
@@ -24,34 +24,41 @@ public class GunItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand); // creates a new ItemStack instance of the user's itemStack in-hand
 
-        if(this.ammunition == 0 && !user.abilities.creativeMode) {
-            user.playSound(SoundEvents.UI_BUTTON_CLICK, 1F,1F);
+        CompoundTag tag = itemStack.getOrCreateTag();
+        int ammo = tag.getInt("ammunition");
+
+        if (ammo == -15 && !user.abilities.creativeMode) {
+            user.playSound(SoundEvents.UI_BUTTON_CLICK, 1F, 1F);
             user.sendMessage(new TranslatableText("item.curry.pistol.no_ammo"), true);
             return TypedActionResult.fail(itemStack);
-        }
-        else if(world.isClient && !user.abilities.creativeMode) {
-            //itemStack.
-            this.ammunition -= 1;
-            user.sendMessage(new TranslatableText("item.curry.pistol.ammo_left").append(String.valueOf(this.ammunition)), true);
-        }
+        } else {
+            if (world.isClient && !user.abilities.creativeMode) {
 
-        world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_GENERIC_EXPLODE, SoundCategory.BLOCKS, 1F, 2F); // plays a globalSoundEvent
-        user.getItemCooldownManager().set(this, 20); //cooldown to use it again
+                System.out.println(ammo + 15);
+                ammo--;
+                tag.putInt("ammunition", ammo);
+                itemStack.setTag(tag);
+                user.sendMessage(new TranslatableText("item.curry.pistol.ammo_left").append(String.valueOf(ammo)), true);
+            }
+
+            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_GENERIC_EXPLODE, SoundCategory.BLOCKS, 1F, 2F); // plays a globalSoundEvent
+            user.getItemCooldownManager().set(this, 20); //cooldown to use it again
         /*
 		user.getItemCooldownManager().set(this, 5);
 		Optionally, you can add a cooldown to your item's right-click use, similar to Ender Pearls.
 		*/
-        if (!world.isClient) {
-            GunProjectile gunProjectile = new GunProjectile(world, user);
-            gunProjectile.setItem(itemStack);
-            gunProjectile.setProperties(user, user.pitch, user.yaw, 0.0F, 5.5F, 0F);
-            world.spawnEntity(gunProjectile); // spawns entity
+            if (!world.isClient) {
+                GunProjectile gunProjectile = new GunProjectile(world, user);
+                gunProjectile.setItem(itemStack);
+                gunProjectile.setProperties(user, user.pitch, user.yaw, 0.0F, 5.5F, 0F);
+                world.spawnEntity(gunProjectile); // spawns entity
+            }
+
+            user.incrementStat(Stats.USED.getOrCreateStat(this));
+
+
+            return TypedActionResult.success(itemStack, world.isClient());
+
         }
-
-        user.incrementStat(Stats.USED.getOrCreateStat(this));
-
-
-        return TypedActionResult.success(itemStack, world.isClient());
     }
-
 }
